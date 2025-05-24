4d746274faa1501906365e649049d05f40e97485
DanielMartiins <danieldemoraiis@gmail.com>
2025-05-24 00:53:04 -0300

    Usado SCSS para organizar melhor o css

--- src/components/ListaTransacoes.vue
+++ src/components/ListaTransacoes.vue
@@ -6,127 +6,134 @@ import { onMounted } from 'vue';
 const listaTransacoes = ref(null)
 
 const props = defineProps({
-    transacoes: {
-        type: Array,
-        required: true,
-    }
+  transacoes: {
+    type: Array,
+    required: true,
+  }
 });
 
 function removerTransacao(index) {
-    props.transacoes.splice(index, 1);
+  props.transacoes.splice(index, 1);
 }
 
 function trocarTransacoes(index_t1, index_t2) {
-    let temp = props.transacoes[index_t1];
-    props.transacoes[index_t1] = props.transacoes[index_t2];
-    props.transacoes[index_t2] = temp;
+  let temp = props.transacoes[index_t1];
+  props.transacoes[index_t1] = props.transacoes[index_t2];
+  props.transacoes[index_t2] = temp;
 }
 
 function scrollarListaParaBaixo() {
-    listaTransacoes.value.scrollTop = listaTransacoes.value.scrollHeight;
+  listaTransacoes.value.scrollTop = listaTransacoes.value.scrollHeight;
 }
 
 const saldoAposTransacao = computed(() => {
-    let saldo = 0.0;
-    let saldoAcumulado = [];
-    for (let i = 0; i < props.transacoes.length; i++)
-        saldoAcumulado.push(saldo += props.transacoes[i].valor)
-    return saldoAcumulado;
+  let saldo = 0.0;
+  let saldoAcumulado = [];
+  for (let i = 0; i < props.transacoes.length; i++)
+    saldoAcumulado.push(saldo += props.transacoes[i].valor)
+  return saldoAcumulado;
 })
 
 onMounted(() => {
-    scrollarListaParaBaixo();
+  scrollarListaParaBaixo();
 })
 
 </script>
 
 <template>
-    <section id="lista-transacoes">
-        <h1>Lista de Transações</h1>
-        <div id="legenda-transacao" >
-            <div>
-                <span>Descrição</span>
-                <span>Transação</span>
-                <span>Saldo atual</span>
-            </div>
-        </div>
-        <ul ref="listaTransacoes" style="list-style-type: none">
-            <li v-for="(t, index) in transacoes" :key="index">
-                <span v-text="t.descricao"></span>
-                <span v-bind:class="t.valor >= 0 ? 'credito' : 'debito'"
-                    v-text="t.operacao + 'R$ ' + Math.abs(t.valor).toFixed(2)"></span>
-                <span v-bind:class="saldoAposTransacao[index] >= 0 ? 'saldo-positivo' : 'saldo-negativo'"
-                    v-text="'R$ ' + saldoAposTransacao[index].toFixed(2)"></span>
-                <span class="seta-troca-transacao">
-                    <button @click="trocarTransacoes(index, index - 1)" :disabled="index == 0">&#8593;</button>
-                    <button @click="trocarTransacoes(index, index + 1)"
-                        :disabled="index === transacoes.length - 1">&#8595;</button>
-                </span>
-                <button @click="removerTransacao(index)">Remover</button>
-            </li>
-        </ul>
-    </section>
+  <section id="lista-transacoes">
+    <h1>Lista de Transações</h1>
+    <div id="legenda-transacao">
+      <div>
+        <span>Descrição</span>
+        <span>Transação</span>
+        <span>Saldo atual</span>
+      </div>
+    </div>
+    <ul ref="listaTransacoes" style="list-style-type: none">
+      <li v-for="(t, index) in transacoes" :key="index">
+        <span v-text="t.descricao"></span>
+        <span v-bind:class="t.valor >= 0 ? 'credito' : 'debito'"
+          v-text="t.operacao + 'R$ ' + Math.abs(t.valor).toFixed(2)">
+        </span>
+        <span v-bind:class="saldoAposTransacao[index] >= 0 ? 'saldo-positivo' : 'saldo-negativo'"
+          v-text="'R$ ' + saldoAposTransacao[index].toFixed(2)">
+        </span>
+        <span class="seta-troca-transacao">
+          <button @click="trocarTransacoes(index, index - 1)" :disabled="index == 0">
+            &#8593;
+          </button>
+          <button @click="trocarTransacoes(index, index + 1)" :disabled="index === transacoes.length - 1">
+            &#8595;
+          </button>
+        </span>
+        <button @click="removerTransacao(index)">Remover</button>
+      </li>
+    </ul>
+  </section>
 </template>
 
 
-<style scoped>
+<style scoped lang="scss">
 
 button {
-    margin-left: auto;
+  margin-left: auto;
 }
 
 li,
 #legenda-transacao {
-    display: flex;
-    justify-content: flex-start;
-    overflow-wrap: break-word;
-    align-items: center;
-    height: 50px;
-    background-color: rgb(36, 41, 57);
+  display: flex;
+  justify-content: flex-start;
+  overflow-wrap: break-word;
+  align-items: center;
+  height: 50px;
+  background-color: rgb(36, 41, 57);
 }
 
 #legenda-transacao {
-    outline: 1px solid;
-    margin: 0px;
+  outline: 1px solid;
+  margin: 0px;
 
-}
-
-#legenda-transacao div {
+  div {
     width: 100%;
     display: inherit;
-}
+  }
 
-#legenda-transacao span {
+  span {
     font-size: 18px;
     font-weight: bold;
     text-decoration: underline;
+  }
 }
 
+
 .seta-troca-transacao button {
-    width: 20px;
-    font-size: 20px;
+  width: 20px;
+  font-size: 20px;
 }
 
 .credito {
-    color: rgb(115, 255, 115);
+  color: rgb(115, 255, 115);
 }
 
 .debito {
-  color: rgb(255, 115, 115);;
-}
-.saldo-negativo {
-    background-color: darkred;
+  color: rgb(255, 115, 115);
+  ;
 }
 
 .saldo-negativo {
-    background-color: darkred;
-    color: yellow;
-    font-weight: bold;
+  background-color: darkred;
+}
+
+.saldo-negativo {
+  background-color: darkred;
+  color: yellow;
+  font-weight: bold;
 }
 
 .transacao-selecionada {
-    border: 2px solid rgb(249, 255, 164);
-    border-radius: 10px;
-    margin: 5px;
+  border: 2px solid rgb(249, 255, 164);
+  border-radius: 10px;
+  margin: 5px;
 }
 </style>
--- src/components/NovaTransacao.vue
+++ src/components/NovaTransacao.vue
@@ -1,97 +1,155 @@
 <script setup>
-import { CREDITO, DEBITO } from "../utils/variaveis-globais";
-import { ref } from "vue";
-
-
+import { CREDITO, DEBITO } from '../utils/variaveis-globais'
+import { ref } from 'vue'
 
 const props = defineProps({
-    transacoes: {
-        type: Array,
-        required: true,
-    }
+  transacoes: {
+    type: Array,
+    required: true,
+  },
 })
 const novaTransacao = ref({
-    descricao: "",
-    valor: NaN,
-    ehValido: true,
+  descricao: '',
+  valor: NaN,
+  ehValido: true,
 })
 
-
-
 function definirOperacao(valorTransacao) {
-    if (valorTransacao >= 0) return CREDITO;
-    else return DEBITO;
+  if (valorTransacao >= 0) return CREDITO
+  else return DEBITO
 }
 
 function resetarFormulario() {
-    novaTransacao.value.descricao = "";
-    novaTransacao.value.valor = NaN;
-    novaTransacao.value.ehValido = true;
+  novaTransacao.value.descricao = ''
+  novaTransacao.value.valor = NaN
+  novaTransacao.value.ehValido = true
 }
 
 function adicionarTransacao() {
-    let valorTransacao = parseFloat(novaTransacao.value.valor);
-    let descricaoTransacao = novaTransacao.value.descricao;
+  let valorTransacao = parseFloat(novaTransacao.value.valor)
+  let descricaoTransacao = novaTransacao.value.descricao
 
-    if (descricaoTransacao.trim() === "" || isNaN(valorTransacao))
-        novaTransacao.value.ehValido = false;
-    else {
-        let transacao = {
-            descricao: descricaoTransacao,
-            valor: valorTransacao,
-            operacao: definirOperacao(valorTransacao),
-        }
-        props.transacoes.push(transacao);
-        resetarFormulario();
+  if (descricaoTransacao.trim() === '' || isNaN(valorTransacao))
+    novaTransacao.value.ehValido = false
+  else {
+    let transacao = {
+      descricao: descricaoTransacao,
+      valor: valorTransacao,
+      operacao: definirOperacao(valorTransacao),
     }
+    props.transacoes.push(transacao)
+    resetarFormulario()
+  }
 }
 
-
-resetarFormulario();
+resetarFormulario()
 </script>
 
 <template>
-    <section id="nova-transacao">
-        <h1>Nova transação</h1>
+  <div id="nova-transacao">
+    <section>
+      <h1>Nova transação</h1>
 
-        <div>
-            <label for="nova-transacao__descricao">Descricao</label>
-            <input id="nova-transacao__descricao" type="text" v-model="novaTransacao.descricao" maxlength="34" />
-        </div>
+      <div class="campo-formulario">
+        <label for="nova-transacao__descricao">Descricao</label>
+        <input id="nova-transacao__descricao" type="text" v-model="novaTransacao.descricao" maxlength="34" />
+      </div>
 
-        <div>
-            <label for="nova-transacao__valor">Valor</label>
-            <input id="nova-transacao__valor" type="number" v-model="novaTransacao.valor" />
+      <div class="campo-formulario">
+        <label for="nova-transacao__valor">Valor</label>
+        <input id="nova-transacao__valor" type="number" v-model="novaTransacao.valor" />
+      </div>
+      <div id="container-adicionar-transacao">
+        <div id="mensagem-erro">
+          <span style="color: red" v-show="!novaTransacao.ehValido">
+            Transação inválida! <br>Verifique os dados e tente novamente
+          </span>
         </div>
-        <span style="color: red" v-if="!novaTransacao.ehValido">
-            Transação inválida! Verifique os dados e tente novamente
-        </span>
-        <button value="Adicionar Transação" @click="adicionarTransacao()">
-            Adicionar Transação
-        </button>
+        <button value="Adicionar Transação" @click="adicionarTransacao()">Adicionar Transação</button>
+      </div>
     </section>
+  </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 #nova-transacao {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: rgb(36, 41, 57);
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  margin-top: 10px;
+  padding: 0;
+  font-size: 18px;
+
+  // * {
+  //   border: 1px solid red;
+  // }
+
+  section {
     display: flex;
     flex-direction: column;
     align-items: center;
     width: 100%;
-    margin-left: auto;
-    margin-right: auto;
-    font-size: 18px;
-    background-color: rgb(36, 41, 57);
-}
 
-h1 {
+  }
+
+  h1 {
     border-bottom: 1px solid white;
     width: 100%;
     margin: 0px;
+    align-self: flex-start;
+
+  }
+
+  div {
+    width: 60%;
+  }
 }
 
-#nova-transacao input,
-#nova-transacao button {
+.campo-formulario {
+  display: flex;
+  text-align: left;
+
+  input {
     width: 300px;
+    margin-left: auto;
+  }
+
+  label {
+    width: 100px;
+  }
 }
+
+#container-adicionar-transacao {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+
+  button {
+    width: 150px;
+  }
+
+  #mensagem-erro {
+    width: 250px;
+    height: 50px;
+    padding: 0;
+    margin: 0;
+
+    span {
+      text-align: left;
+      padding: 0;
+      margin: 0;
+    }
+  }
+}
+
+/* #nova-transacao input,
+#nova-transacao button, #nova-transacao input {
+    width: 300px;
+} */
 </style>
